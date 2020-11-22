b30a69f1b184e7070964b543617eeee05f7281cf
Gabriellji <gabriellji@yahoo.com>
2020-11-22 17:17:08 +0300

    feat: test fetch method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,48 @@
-fetch('https://swapi.dev/api/people/1/')
-  .then((res) => {
-    console.log('got response', res.status)
+class SwapiService {
+  _apiBase = 'https://swapi.dev/api';
+
+  async getResourse(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` +
+        `, received ${res.status}`)
+    }
+    return res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResourse(`/people/`)
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResourse(`/people/${id}`)
+  }
+
+  async getAllPLanets() {
+    const res = await this.getResourse(`/planets/`)
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResourse(`/starships/${id}`)
+  }
+
+  async getAllStarships() {
+    const res = await this.getResourse(`/starships/`)
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResourse(`/planets/${id}`)
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPLanets().then((people) => {
+  people.forEach((data) => {
+    console.log(data.name);
   })
+})
